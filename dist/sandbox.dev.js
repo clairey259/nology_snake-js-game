75285327eb0bfb3e9f27d2a4de2f7db85dfa8140
Claire <claire.huizenga@outlook.com>
2022-10-10 21:13:35 +1100

    deleted .dist

--- dist/sandbox.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var rand = Math.random() * (500 - 20) + 10;
-devisablebyten = rand % 10;
-console.log(devisablebyten);
-console.log(Math.Toint(devisablebyten));
-console.log(Math.round(rand)); // var randomY = Math.trunc(Math.random() * (canvas.height - 20) + 10);
-// var cherry = new BasicBox(randomX, randomY, 10, 10, "red");
-// console.log(cherry)
